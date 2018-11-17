fa762f95a14bdf148bd2f6e5efe978a101681a77
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2018-11-17 13:59:55 +0100

    Fix too large xshift
    Signed-off-by: Jesse van Rhijn <jesse.v.rhijn@gmail.com>

--- lib/renderer/text/string.cc
+++ lib/renderer/text/string.cc
@@ -29,10 +29,11 @@ void String::Center() {
   const auto last_char = characters_.back();
   const auto loc = last_char.location();
   // only need first and last char for x shift
-  const double xshift = (-0.5*(loc.first + last_char.Width()) - first_char.bearing().first)*scale_;
+  const double xshift = -0.5*(loc.first + last_char.Width())*scale_;
   // all numbers are equally tall; grab last char arbitrarily
   const double yshift = -0.5*last_char.Height()*scale_;
-  std::for_each(characters_.begin(), characters_.end(), [&xshift, &yshift](Character& c){c.MoveAlong(xshift, yshift);});
+  std::for_each(characters_.begin(), characters_.end(),
+                [&xshift, &yshift](Character& c){c.MoveAlong(xshift, yshift);});
 }
 
 void String::Draw(GLFWwindow *window) const {
